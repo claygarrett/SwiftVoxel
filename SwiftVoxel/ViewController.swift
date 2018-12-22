af8b442f05840e548ce07c8ab6c7862b24dac5f7
Clay Garrett <claygarrett@gmail.com>
2018-12-22 14:01:18 +0100

    Initial Commit

---
+++ SwiftVoxel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVoxel
+//
+//  Created by Clay Garrett on 12/22/18.
+//  Copyright © 2018 Clay Garrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
